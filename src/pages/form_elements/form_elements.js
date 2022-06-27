2b9a6bd2d67aa82a44aa71593eba433613efc8ae
vladislav <vladislavsokolov019@gmail.com>
2022-06-28 06:15:13 +0700

    create input block

---
+++ src/pages/form_elements/form_elements.js
@@ -0,0 +1,5 @@
+import './form_elements.pug'
+import './form_elements.less'
+
+import '../../components/blocks/ui-logo/ui-logo.js'
+import '../../components/blocks/input-block/input-block.js' 
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,46 @@
+//requires
+const fs = require('fs')
 const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-let mode = "development"
-if (process.env.NODE_ENV === 'production') {
-	mode = "production"
+//paths
+const PATHS = {
+  src: path.resolve(__dirname, './src'),
+  dist: path.resolve(__dirname, './dist'),
 }
+const PAGES_DIR = path.resolve(__dirname, 'src/pages');
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .map(item => item.replace(/\.[^/.]+$/, ''))
+  .filter(item => item != 'layouts');
+
+//pages plugin
+
+const sitePages = PAGES
+  .map(name => new HtmlWebpackPlugin({
+    template: `./src/pages/${name}/${name}.pug`,
+    filename: `./pages/${name}/${name}.html`,
+    chunks: [name],
+  }));
+
+//mode
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+	mode = 'production'
+}
+console.log("pages :\n" ,sitePages)
 console.log(mode + ' mode')
 
 module.exports = {
-
 	mode: mode,
 	entry: {
-		colors_and_types: './src/pages/colors_and_types/colors_and_types.js'
+		colors_and_types: './src/pages/colors_and_types/colors_and_types.js',
+		form_elements: './src/pages/form_elements/form_elements.js',
 	},
 	output: {
-		filename: './pages/[name]/[name].[contenthash].js',
-		assetModuleFilename: 'assets/images/[name][ext]',
+		filename: './pages/[name]/[name].js',
 		clean: true,
 	},
 	optimization: {
@@ -26,14 +50,12 @@ module.exports = {
 	},
 
 	plugins: [
+		// new BundleAnalyzerPlugin(),
 		new MiniCssExtractPlugin({
-			filename: './pages/[name]/[name].[contenthash].css',
+			filename: './pages/[name]/[name].css',
 		}),
-		new HtmlWebpackPlugin({
-			template: './src/pages/colors_and_types/colors_and_types.pug',
-			filename: './pages/[name]/[name].html'
-		}),
-	],
+	]
+	.concat(sitePages),
 
 	module: {
 		rules: [
@@ -50,36 +72,36 @@ module.exports = {
 			{
 				test: /\.(le|c)ss$/,
 				use: [
-					(mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+					mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
 					'css-loader',
 					'postcss-loader',
 					'less-loader'
 				],
 			},
 			{
-       			test: /\.(png|svg|jpg|jpeg|gif)$/,
-    		    type: 'asset/resource',
-    		    generator: {filename: 'assets/images/[name][ext]'}
-      		},
+				test: /\.(png|svg|jpg|jpeg|gif)$/,
+				type: 'asset/resource',
+				generator: {filename: 'assets/images/[name][ext]'}
+			},
 			{
 				test: /\.(woff|woff2|eot|ttf|otf)$/,
 				type: 'asset/resource',
 				generator: {filename: 'assets/fonts/[name][ext]'},
 			},
 			{
-      			test: /\.pug$/,
-      			use:[
-      				'@webdiscus/pug-loader',
-      			] 
-      		},
+ 				test: /\.pug$/,
+				use:[
+					'@webdiscus/pug-loader',
+				] 
+			},
 		],
 	},
 
 	devServer: {
-  		static: {
-  	    	directory: path.join(__dirname, 'dist'),
-  	  	},
-  	  	compress: true,
-  	  	port: 8080,
-  	},
+		static: {
+			directory: PATHS.dist,
+		},
+		compress: true,
+		port: 8080,
+	},
 }
