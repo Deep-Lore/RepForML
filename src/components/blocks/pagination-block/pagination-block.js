094b02870f645d255e59d2fab0bac177660affef
vladislav <vladislavsokolov019@gmail.com>
2022-07-12 20:37:41 +0700

    added js from pagination block

--- src/components/blocks/pagination-block/pagination-block.js
+++ src/components/blocks/pagination-block/pagination-block.js
@@ -1,12 +1,12 @@
 import './pagination-block.less'
 
-const arr = document.querySelectorAll('.pagination-block__page-num')
-var activeElem = arr[0]
+const arr = document.querySelectorAll('.pagination-block__page-item_clickeble')
+var activeElem = document.querySelector('.pagination-block__page-item_selected')
 
 arr.forEach((selectedElem) => {
 	selectedElem.addEventListener('click', () => {
-		activeElem.classList.remove('pagination-block__page-num_selected')
-		selectedElem.classList.add('pagination-block__page-num_selected')
+		activeElem.classList.remove('pagination-block__page-item_selected')
+		selectedElem.classList.add('pagination-block__page-item_selected')
 		activeElem = selectedElem
 	})
 })
