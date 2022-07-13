37b18d02ec0fffc388d3c8e550f4bd2183fdc825
vladislav <vladislavsokolov019@gmail.com>
2022-07-13 22:59:31 +0700

    added js from range slider

--- src/components/blocks/range-slider-block/range-slider-block.js
+++ src/components/blocks/range-slider-block/range-slider-block.js
@@ -1,2 +1,40 @@
 import './range-slider-block.less'
 
+var block = document.querySelector('.range-slider-block')
+var blockLine = document.querySelector('.range-slider-block__line')
+var thumbEl = document.querySelector('.range-slider-block__thumb')
+var thumbLeftEl = document.querySelector('.range-slider-block__thumb-left')
+var thumbRightEl = document.querySelector('.range-slider-block__thumb-right')
+
+
+// console.log(thumbLeftEl.position,thumbRightEl.position)
+
+thumbLeftEl.addEventListener('mousedown', (e) => {
+	document.onmousemove = (e) => {
+		thumbLeftEl.style.left = e.pageX - block.getBoundingClientRect().left - thumbEl.offsetHeight/2 + 'px'
+		blockLine.style.left = e.pageX - block.getBoundingClientRect().left + 'px'
+		if(e.pageX - block.getBoundingClientRect().left - thumbEl.offsetHeight/2 < 0){
+			thumbLeftEl.style.left = '0px'
+			blockLine.style.left = thumbEl.offsetHeight/2 + 'px'
+		}
+	}
+	document.onmouseup = (e) => {
+		document.onmousemove = null
+		thumbLeftEl.onmouseup = null
+	}
+})
+
+thumbRightEl.addEventListener('mousedown', (e) => {
+	document.onmousemove = (e) => {
+		thumbRightEl.style.right = block.getBoundingClientRect().right - e.pageX - thumbEl.offsetHeight/2 + 'px'
+		blockLine.style.right = block.getBoundingClientRect().right - e.pageX + 'px'
+		if(block.getBoundingClientRect().right - e.pageX - thumbEl.offsetHeight/2 < 0){
+			thumbRightEl.style.right = '0px'
+			blockLine.style.right = thumbEl.offsetHeight/2 + 'px'
+		}
+	}
+	document.onmouseup = (e) => {
+		document.onmousemove = null
+		thumbRightEl.onmouseup = null
+	}
+})
