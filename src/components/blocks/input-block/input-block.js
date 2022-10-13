78c69576931fea18d630375e6317a2752f9957a0
vladislav <vladislavsokolov019@gmail.com>
2022-10-14 00:43:30 +0700

    work on input block

--- src/components/blocks/input-block/input-block.js
+++ src/components/blocks/input-block/input-block.js
@@ -2,38 +2,37 @@ import './input-block.less'
 
 var inputBlocks = document.querySelectorAll('.input-block')
 
-console.log(inputBlocks)
-
 inputBlocks.forEach(inputBlock => {
     //block vars
     var inputBlockItems = inputBlock.querySelector('.input-block__input-items')
     var inputBlockDropdown = inputBlock.querySelector('.input-block__dropdown')
     var paragraphs = inputBlockDropdown.querySelectorAll('.input-block__dropdown-paragraph')
+    //arrow
+    var arrow = inputBlockItems.querySelector('.input-block__arrow')
     //button vars
     var btnMinus
     var btnPlus
     var counter
+    
 
     //body//
     //open dropdown
-    inputBlockItems.addEventListener('focusin', addDropdown)
+    inputBlockItems.addEventListener('click', toggleDropdown)
 
     //close dropdown
-    inputBlockItems.addEventListener('mouseover', () => {
-        console.log(1)
-        inputBlockItems.removeEventListener('focusout', removeDropdown)
-    })
-    inputBlockItems.addEventListener('mouseout', () => {
-        console.log(2)
-        inputBlockItems.addEventListener('focusout', removeDropdown)
 
+    document.addEventListener('mousedown', (e) => {
+        if (!e.composedPath().includes(inputBlockItems)) {
+            removeDropdown()
+        }
     })
 
     //buttons
     paragraphs.forEach(paragraph => {
-        counter = 0
         btnMinus = paragraph.querySelector('.input-block__dropdown-button_type_minus')
         btnPlus = paragraph.querySelector('.input-block__dropdown-button_type_plus')
+        //counter
+        counter = 0
         
         btnMinus.addEventListener('click', (e) => {
             e.target.parentNode.querySelector('.input-block__dropdown-button-text').innerHTML--
@@ -44,12 +43,12 @@ inputBlocks.forEach(inputBlock => {
     })
 
     //functions//
-    function addDropdown() {
-        inputBlockDropdown.classList.add('input-block__dropdown_visible')
-        console.log(3)
+    function toggleDropdown() {
+        inputBlockDropdown.classList.toggle('input-block__dropdown_visible')
+        arrow.classList.toggle('input-block__arrow_type_dropdown-active')
     }
     function removeDropdown() {
         inputBlockDropdown.classList.remove('input-block__dropdown_visible')
-        console.log(4)
+        arrow.classList.remove('input-block__arrow_type_dropdown-active')
     }
 })
