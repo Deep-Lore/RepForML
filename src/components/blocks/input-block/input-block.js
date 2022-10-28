4b48ea254d01a356ca723502408f3a51ee466969
vladislav <vladislavsokolov019@gmail.com>
2022-10-29 03:26:31 +0700

    revision input-block

--- src/components/blocks/input-block/input-block.js
+++ src/components/blocks/input-block/input-block.js
@@ -2,76 +2,82 @@ import './input-block.less'
 
 const maxInputLength =  15
 
-var inputBlocks = document.querySelectorAll('.input-block')
+const inputBlocks = document.querySelectorAll('.input-block')
 
 inputBlocks.forEach(inputBlock => {
-    //block vars
-    let inputBlockItems = inputBlock.querySelector('.input-block__input-items')
-    let inputBlockItem = inputBlock.querySelector('.input-block__input-item')
-    let inputBlockDropdown = inputBlock.querySelector('.input-block__dropdown')
-    let paragraphs = inputBlockDropdown.querySelectorAll('.input-block__dropdown-paragraph')
-    //placeholder
-    let placeHolderDefaultText = inputBlockItem.placeholder
-    //arrow
-    let arrow = inputBlockItems.querySelector('.input-block__arrow')
-    //button vars
-    let btnMinus
-    let btnPlus
-    let counter
-    //open dropdown
-    inputBlockItems.addEventListener('click', toggleDropdown)
+    if (inputBlock.contains(inputBlock.querySelector('.input-block__dropdown'))) {
+        //block vars
+        let inputBlockItems = inputBlock.querySelector('.input-block__input-items')
+        let inputBlockItem = inputBlock.querySelector('.input-block__input-item')
+        let inputBlockDropdown = inputBlock.querySelector('.input-block__dropdown')
+        let paragraphs = inputBlockDropdown.querySelectorAll('.input-block__dropdown-paragraph')
+        //placeholder
+        let placeHolderDefaultText = inputBlockItem.placeholder
+        //arrow
+        let arrow = inputBlockItems.querySelector('.input-block__arrow')
+        //button vars
+        let btnMinus
+        let btnPlus
+        let counter
+        //open dropdown
+        inputBlockItems.addEventListener('click', toggleDropdown)
 
-    // close dropdown
-    document.addEventListener('mousedown', (e) => {
-        if (!(e.composedPath().includes(inputBlockItems)||e.composedPath().includes(inputBlockDropdown))) {
-            removeDropdown()
-        }
-    })
-
-    // buttons //
-    paragraphs.forEach(paragraph => {
-        //buttons
-        btnMinus = paragraph.querySelector('.input-block__dropdown-button_type_minus')
-        btnPlus = paragraph.querySelector('.input-block__dropdown-button_type_plus')
-        //counter
-        counter = 0
-
-        //place 0 to paragraph
-        btnMinus.classList.add('input-block__dropdown-button_disabled')
-
-        //btn minus
-        btnMinus.addEventListener('click', (e) => {
-            counter = e.target.parentNode.querySelector('.input-block__dropdown-button-text')
-            if (counter.innerHTML>0&&counter.innerHTML<=4) {
-                counter.innerHTML--
-                //change placeholder
-                createPlaceholder()
-                //button minus enable
-                buttonEnable(e, 'plus')
-            }if (counter.innerHTML==0){
-                buttonDisable(e, 'minus')
-            } 
+        // close dropdown
+        document.addEventListener('mousedown', (e) => {
+            if (!(e.composedPath().includes(inputBlockItems)||e.composedPath().includes(inputBlockDropdown))) {
+                removeDropdown()
+            }
         })
-        //btn plus
-        btnPlus.addEventListener('click', (e) => {
-            counter = e.target.parentNode.querySelector('.input-block__dropdown-button-text')
-            if (counter.innerHTML>=0&&counter.innerHTML<4) {
-                counter.innerHTML++
-                //change placeholder
-                createPlaceholder()
-                //button minus enable
-                buttonEnable(e, 'minus')
-            }if (counter.innerHTML==4){
-                buttonDisable(e, 'plus')
-            } 
-        })
-    })
 
-    //Clear and apply buttons
-    if (inputBlock.classList.contains('input-block_size_big')) {
+        // buttons //
+        paragraphs.forEach(paragraph => {
+            //buttons
+            btnMinus = paragraph.querySelector('.input-block__dropdown-button_type_minus')
+            btnPlus = paragraph.querySelector('.input-block__dropdown-button_type_plus')
+            //counter
+            counter = 0
+
+            //place 0 to paragraph
+            btnMinus.classList.add('input-block__dropdown-button_disabled')
+
+            //btn minus
+            btnMinus.addEventListener('click', (e) => {
+                counter = e.target.parentNode.querySelector('.input-block__dropdown-button-text')
+                if (counter.innerHTML>0&&counter.innerHTML<=4) {
+                    counter.innerHTML--
+                    //change placeholder
+                    createPlaceholder()
+                    //button minus enable
+                    buttonEnable(e, 'plus')
+                }if (counter.innerHTML==0){
+                    buttonDisable(e, 'minus')
+                }
+                if (numberNonZeroParagraphs()==0) {
+                    removeButtonClear()
+                }
+            })
+            //btn plus
+            btnPlus.addEventListener('click', (e) => {
+                counter = e.target.parentNode.querySelector('.input-block__dropdown-button-text')
+                if (counter.innerHTML>=0&&counter.innerHTML<4) {
+                    counter.innerHTML++
+                    //change placeholder
+                    createPlaceholder()
+                    //button minus enable
+                    buttonEnable(e, 'minus')
+                }if (counter.innerHTML==4){
+                    buttonDisable(e, 'plus')
+                }
+                if (numberNonZeroParagraphs()!=0) {
+                    addButtonClear()
+                }
+            })
+        })
+
+        //Clear and apply buttons
         let buttonClear = inputBlockDropdown.querySelector('.input-block__button-clear')
         let buttonApply = inputBlockDropdown.querySelector('.input-block__button-apply')
-
+        //button clear
         buttonClear.addEventListener('click', () => {
             paragraphs.forEach(paragraphName => {
                 paragraphName.querySelector('.input-block__dropdown-button-text').innerHTML=0
@@ -84,63 +90,77 @@ inputBlocks.forEach(inputBlock => {
             })
             createPlaceholder()
             inputBlockItem.placeholder = placeHolderDefaultText //get default placeholder
+            removeButtonClear()
         })
-    }
 
-    // functions //
-    function toggleDropdown() {
-        inputBlockItems.classList.toggle('input-block__input-items_dropdown-active')
-        arrow.classList.toggle('input-block__arrow_type_dropdown-active')
-        inputBlockDropdown.classList.toggle('input-block__dropdown_dropdown-active')
-    }
-    function removeDropdown() {
-        inputBlockItems.classList.remove('input-block__input-items_dropdown-active')
-        arrow.classList.remove('input-block__arrow_type_dropdown-active')
-        inputBlockDropdown.classList.remove('input-block__dropdown_dropdown-active')
-    }
-    //placeholder
-    function createPlaceholder() {
-        clearPlaceHolder()
-        if (inputBlockItem.dataset.placeholderMode=='default') { 
-            let sum = 0 //create counter
-            paragraphs.forEach((paragraphName, i, parArr) => {
-                if (inputBlockItem.placeholder.length<maxInputLength) {
-                    if (paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML!=0) {
-                        if (inputBlockItem.placeholder != 0) {
-                            inputBlockItem.placeholder += ', '
-                        }
-                        inputBlockItem.placeholder += `${paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML} ${paragraphs[i].querySelector('.input-block__dropdown-paragraph-text').innerHTML}`
-                    }
-                }
-                sum += Number(paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML)
-            })
-            if (sum==0) inputBlockItem.placeholder = placeHolderDefaultText
-            else inputBlockItem.placeholder += '...'
-        } 
-        if (inputBlockItem.dataset.placeholderMode=='count') {
-            let sum = 0 //create counter
-            paragraphs.forEach((paragraphName, i, parArr) => {
-                sum += Number(paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML)
-            })
-
-            //install placeholder
-            inputBlockItem.placeholder = sum
-            if (sum==0) inputBlockItem.placeholder = placeHolderDefaultText
-            else if (sum==1) inputBlockItem.placeholder += ' гость'
-            else if (sum>1&&sum<5) inputBlockItem.placeholder += ' гостя'
-            else if (sum>=5) inputBlockItem.placeholder += ' гостей'
+        // functions //
+        function toggleDropdown() {
+            inputBlockItems.classList.toggle('input-block__input-items_dropdown-active')
+            arrow.classList.toggle('input-block__arrow_type_dropdown-active')
+            inputBlockDropdown.classList.toggle('input-block__dropdown_dropdown-active')
         }
-    }
-    function clearPlaceHolder() {
-        inputBlockItem.placeholder = ''
-    }
-    //button disabled
-    function buttonEnable(e, sign) { //sign = plus or minus
-        let button = e.target.parentNode.querySelector(`.input-block__dropdown-button_type_${sign}`)
-        button.classList.remove('input-block__dropdown-button_disabled')
-    }
-    function buttonDisable(e, sign) { //sign = plus or minus
-        let button = e.target.parentNode.querySelector(`.input-block__dropdown-button_type_${sign}`)
-        button.classList.add('input-block__dropdown-button_disabled')
+        function removeDropdown() {
+            inputBlockItems.classList.remove('input-block__input-items_dropdown-active')
+            arrow.classList.remove('input-block__arrow_type_dropdown-active')
+            inputBlockDropdown.classList.remove('input-block__dropdown_dropdown-active')
+        }
+        //placeholder
+        function createPlaceholder() {
+            clearPlaceHolder()
+            if (inputBlockItem.dataset.placeholderMode=='default') { 
+                let sum = 0 //create counter
+                paragraphs.forEach((paragraphName, i, parArr) => {
+                    if (inputBlockItem.placeholder.length<maxInputLength) {
+                        if (paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML!=0) {
+                            if (inputBlockItem.placeholder != 0) {
+                                inputBlockItem.placeholder += ', '
+                            }
+                            inputBlockItem.placeholder += `${paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML} ${paragraphs[i].querySelector('.input-block__dropdown-paragraph-text').innerHTML}`
+                        }
+                    }
+                    sum += Number(paragraphs[i].querySelector('.input-block__dropdown-button-text').innerHTML)
+                })
+                if (sum==0) inputBlockItem.placeholder = placeHolderDefaultText
+                else inputBlockItem.placeholder += '...'
+            } 
+            if (inputBlockItem.dataset.placeholderMode=='counter') {
+                //install placeholder
+                let sum = numberNonZeroParagraphs()
+                inputBlockItem.placeholder = sum
+                if (sum==0) inputBlockItem.placeholder = placeHolderDefaultText
+                else if (sum==1) inputBlockItem.placeholder += ' гость'
+                else if (sum>1&&sum<5) inputBlockItem.placeholder += ' гостя'
+                else if (sum>=5) inputBlockItem.placeholder += ' гостей'
+            }
+        }
+        function clearPlaceHolder() {
+            inputBlockItem.placeholder = ''
+        }
+        //button disabled
+        function buttonEnable(e, sign) { //sign = plus or minus
+            let button = e.target.parentNode.querySelector(`.input-block__dropdown-button_type_${sign}`)
+            button.classList.remove('input-block__dropdown-button_disabled')
+        }
+        function buttonDisable(e, sign) { //sign = plus or minus
+            let button = e.target.parentNode.querySelector(`.input-block__dropdown-button_type_${sign}`)
+            button.classList.add('input-block__dropdown-button_disabled')
+        }
+        //number of non-zero paragraphs
+        function numberNonZeroParagraphs() {
+            let counter = 0
+            paragraphs.forEach(paragraph => {
+                counter += Number(paragraph.querySelector('.input-block__dropdown-button-text').innerHTML)
+            })
+            return counter
+        }
+        //button clear
+        function addButtonClear() {
+            let buttonClear = inputBlockDropdown.querySelector('.input-block__button-clear')
+            buttonClear.classList.remove('input-block__button-clear_hided')
+        }
+        function removeButtonClear() {
+            let buttonClear = inputBlockDropdown.querySelector('.input-block__button-clear')
+            buttonClear.classList.add('input-block__button-clear_hided')
+        }
     }
 })
