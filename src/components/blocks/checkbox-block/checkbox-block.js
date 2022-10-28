83e0594d1fded386f96a0f5e78b3704f7f7dfc18
vladislav <vladislavsokolov019@gmail.com>
2022-10-28 20:43:59 +0700

    created expandable mode

--- src/components/blocks/checkbox-block/checkbox-block.js
+++ src/components/blocks/checkbox-block/checkbox-block.js
@@ -1,3 +1,14 @@
 import './checkbox-block.less'
 
-// document.querySelectorAll('')
+const checkboxListExpandable = document.querySelectorAll('.checkbox-list_expandable')
+
+checkboxListExpandable.forEach( () => {
+	const checkboxListHeader = document.querySelector('.checkbox-list__header')
+	const checkboxListExpandableArrow = checkboxListHeader.querySelector('.checkbox-list__expandable-arrow')
+	const checkboxListItems = document.querySelector('.checkbox-list__items')
+
+	checkboxListHeader.addEventListener('click', () => {
+		checkboxListItems.classList.toggle('checkbox-list__items_hided')
+		checkboxListExpandableArrow.classList.toggle('checkbox-list__expandable-arrow_closed')
+	})
+})
