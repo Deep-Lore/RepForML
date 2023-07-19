0c440d8d360f628d7d6036ac1f4b99a2e0f63bd6
vladislav <deeplore894@gmail.com>
2023-07-19 09:11:02 +0700

    refactoring register card

--- src/components/blocks/room-search-card/room-search-card.js
+++ src/components/blocks/room-search-card/room-search-card.js
@@ -2,7 +2,5 @@
 import './room-search-card.less'
 
 //import component of other blocks
-// import '../button-block/button-block.js'
-// import '../input-block/input-block.js'
-// import '../radio-block/radio-block.js'
-// import '../toggle-block/toggle-block.js'
+import '../calendar/calendar.js'
+import '../calendar/calendar.less'
